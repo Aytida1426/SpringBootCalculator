dd3582f86a68f8adf7c8423303f93a57b9809af0
AdityaS1426 <adisurapaneni@gmail.com>
2021-03-01 22:34:44 -0800

    Initial commit

---
+++ src/main/java/com/calculator/debugdatabandits/CalculatorSpringBoot.java
@@ -0,0 +1,13 @@
+package com.calculator.debugdatabandits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorSpringBoot.class, args);
+    }
+
+}
---
+++ src/test/java/com/calculator/debugdatabandits/DebugdatabanditsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.debugdatabandits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugdatabanditsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

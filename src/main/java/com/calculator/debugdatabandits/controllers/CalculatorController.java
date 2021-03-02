cbf6e513f66419ed7c836b43dcc14144c80c61ba
AdityaS1426 <adisurapaneni@gmail.com>
2021-03-01 22:42:29 -0800

    Cleaned up CalculatorController.java.

--- src/main/java/com/calculator/debugdatabandits/controllers/CalculatorController.java
+++ src/main/java/com/calculator/debugdatabandits/controllers/CalculatorController.java
@@ -15,71 +15,71 @@ public class CalculatorController {
     @Autowired
     private CalculateSimple calculateSimple;
 
-    @RequestMapping("/calculator")
+    @RequestMapping("/")
     public String getCalculatorPage(Model model){
         model.addAttribute("operationModel",operationModel);
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="add", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="add", method = RequestMethod.POST)
     public String add(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.add(operationModel));
+        model.addAttribute("Result", calculateSimple.add(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="subtract", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="subtract", method = RequestMethod.POST)
     public String subtract(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.subtract(operationModel));
+        model.addAttribute("Result", calculateSimple.subtract(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="multiply", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="multiply", method = RequestMethod.POST)
     public String multiply(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.multiply(operationModel));
+        model.addAttribute("Result", calculateSimple.multiply(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="divide", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="divide", method = RequestMethod.POST)
     public String divide(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.divide(operationModel));
+        model.addAttribute("Result", calculateSimple.divide(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="fibonacci", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="fibonacci", method = RequestMethod.POST)
     public String fibonacci(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.fibonacci(operationModel));
+        model.addAttribute("Result", calculateSimple.fibonacci(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="sqrt", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="sqrt", method = RequestMethod.POST)
     public String sqrt(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.sqrt(operationModel));
+        model.addAttribute("Result", calculateSimple.sqrt(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="power", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="power", method = RequestMethod.POST)
     public String power(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.power(operationModel));
+        model.addAttribute("Result", calculateSimple.power(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="factorial", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="factorial", method = RequestMethod.POST)
     public String factorial(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("result", calculateSimple.factorial(operationModel));
+        model.addAttribute("Result", calculateSimple.factorial(operationModel));
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="clearSimple", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="clearSimple", method = RequestMethod.POST)
     public String clearSimple(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
         model.addAttribute("operationModel",  calculateSimple.clearSimple(operationModel));
-        model.addAttribute("result", 0);
+        model.addAttribute("Result", 0);
         return "Calculator";
     }
 
-    @RequestMapping(value="/calculator", params="clearAdvanced", method = RequestMethod.POST)
+    @RequestMapping(value="/", params="clearAdvanced", method = RequestMethod.POST)
     public String clearAdvanced(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
         model.addAttribute("operationModel",  calculateSimple.clearAdvanced(operationModel));
-        model.addAttribute("result", 0);
+        model.addAttribute("Result", 0);
         return "Calculator";
     }
 }

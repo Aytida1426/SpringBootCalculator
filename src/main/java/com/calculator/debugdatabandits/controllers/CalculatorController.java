12a2eaf356d5a370c52a7bc1ef790231037c6b15
AdityaS1426 <adisurapaneni@gmail.com>
2021-03-01 22:45:18 -0800

    Cleaned up CalculatorController.java.

--- src/main/java/com/calculator/debugdatabandits/controllers/CalculatorController.java
+++ src/main/java/com/calculator/debugdatabandits/controllers/CalculatorController.java
@@ -23,63 +23,63 @@ public class CalculatorController {
 
     @RequestMapping(value="/", params="add", method = RequestMethod.POST)
     public String add(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.add(operationModel));
+        model.addAttribute("result", calculateSimple.add(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="subtract", method = RequestMethod.POST)
     public String subtract(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.subtract(operationModel));
+        model.addAttribute("result", calculateSimple.subtract(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="multiply", method = RequestMethod.POST)
     public String multiply(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.multiply(operationModel));
+        model.addAttribute("result", calculateSimple.multiply(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="divide", method = RequestMethod.POST)
     public String divide(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.divide(operationModel));
+        model.addAttribute("result", calculateSimple.divide(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="fibonacci", method = RequestMethod.POST)
     public String fibonacci(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.fibonacci(operationModel));
+        model.addAttribute("result", calculateSimple.fibonacci(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="sqrt", method = RequestMethod.POST)
     public String sqrt(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.sqrt(operationModel));
+        model.addAttribute("result", calculateSimple.sqrt(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="power", method = RequestMethod.POST)
     public String power(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.power(operationModel));
+        model.addAttribute("result", calculateSimple.power(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="factorial", method = RequestMethod.POST)
     public String factorial(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
-        model.addAttribute("Result", calculateSimple.factorial(operationModel));
+        model.addAttribute("result", calculateSimple.factorial(operationModel));
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="clearSimple", method = RequestMethod.POST)
     public String clearSimple(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
         model.addAttribute("operationModel",  calculateSimple.clearSimple(operationModel));
-        model.addAttribute("Result", 0);
+        model.addAttribute("result", 0);
         return "Calculator";
     }
 
     @RequestMapping(value="/", params="clearAdvanced", method = RequestMethod.POST)
     public String clearAdvanced(@ModelAttribute("operationModel")  OperationModel operationModel, Model model ){
         model.addAttribute("operationModel",  calculateSimple.clearAdvanced(operationModel));
-        model.addAttribute("Result", 0);
+        model.addAttribute("result", 0);
         return "Calculator";
     }
 }
